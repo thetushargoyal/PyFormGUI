1c4bd32e2b46da7dca914efdf1c409a014e8d303
thetushargoyal <tushar.goyal@plaksha.edu.in>
2022-08-10 14:23:29 +0530

    tried something

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from imports import *
 
 top_line_label = tk.Label(window, text="Configure Your Chromite Core", font= "Arial 15 bold").place(relx = 0.5, rely =0.03, anchor = CENTER)
 
-createTextEntry("Number of Harts", window, fixed_val = '', _infobox = True, heading = 'Hello', description = 'What\'s is up', numbers_only=True)
+createTextEntry("No. of Harts", window, fixed_val = '', _infobox = True, heading = 'Hello', description = 'What\'s is up', numbers_only=True)
 
 createTrueFalse("Overlap Redirections", window, _infobox = True, heading= 'Overlap Rediretions', description='Hola gracis')
 
