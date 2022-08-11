4377e1dbb3e17781cf8abedb139668c148ef5cfa
thetushargoyal <tushar.goyal@plaksha.edu.in>
2022-08-11 23:25:42 +0530

    initial

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from modules.imports import *
 
 top_line_label = tk.Label(window, text="Configure Your Chromite Core", font= "Arial 15 bold").place(relx = 0.5, rely =0.03, anchor = CENTER)
 
-createTextEntry("No. of Harts", window, fixed_val = '', _infobox = True, heading = 'Hello', description = 'What\'s is up', numbers_only=True)
-createTrueFalse("Overlap Redirections", window, _infobox = True, heading= 'Overlap Rediretions', description='Hola gracis')
+createTextEntry("No. of Harts", window, fixed_val = '', _infobox = True, heading = 'No. of Harts', description = 'No. of Harts No. of Harts No. of Harts', numbers_only=True)
+createTrueFalse("Overlap Redirections", window, _infobox = True, heading= 'Overlap Rediretions', description='Overlap Rediretions Overlap Rediretions Overlap Rediretions')
 createCounterField(window, 'No. of Times hit', 'Arial 11')
 createDropDown(window, ['-O3', '-O2', '-O1'], 'Optimization Type', 'Arial 11')
-createTextEntry("Text also", window, fixed_val = '', _infobox = True, heading = 'Hello', description = 'What\'s is up')
+createTextEntry("Text also", window, fixed_val = '', _infobox = True, heading = 'Hello', description = 'What\'s is up Overlap Rediretions Overlap Rediretions Overlap Rediretions')
 
 window.mainloop()
